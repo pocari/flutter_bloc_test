05a987d977dd618560476280aecc1dc4d4446ece
pocari <caffelattenonsugar@gmail.com>
2019-11-15 00:14:27 +0900

    Blocサンプル

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,64 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      MaterialApp(
+          title: 'Bloc Example',
+          home: BlocProvider<CounterBloc>(
+            builder: (context) => CounterBloc(),
+            child: CounterScreen(),
+          )),
+    );
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class CounterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    final counterBloc = BlocProvider.of<CounterBloc>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('counter'),
+      ),
+      body: BlocBuilder<CounterBloc, int>(
+        builder: (context, count) {
+          return Center(
+            child: Text(
+              '$count',
+              style: TextStyle(fontSize: 24.0),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                counterBloc.dispatch(CounterEvent.increment);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+enum CounterEvent { increment }
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  @override
+  int get initialState => 0;
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.increment:
+        yield currentState + 1;
+        break;
+    }
   }
 }
